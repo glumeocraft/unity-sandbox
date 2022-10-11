0f31c066ad8cfbeb98d4b9969d8afedc0f9fe6a9
RCINET\doram <dora@marton.nexius.hu>
2022-10-11 16:56:19 +0200

    Added battle simulator backend code

---
+++ BackendFiles/BattleCalculator/Battle.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static BattleCalculator.Program;
+
+namespace BattleCalculator
+{
+    public class Battle
+    {
+        Random random = new Random();
+        public Terrain Terrain { get; set; } = Terrain.Plain;
+
+        public void SimulateBattle(List<Unit> firstPlayerUnits, List<Unit> secondPlayerUnits)
+        {
+            // Get AttackStrength
+            var player1AttackStrength = SumStrengthOfAPlayer(firstPlayerUnits, Moves.Attack);
+            Console.WriteLine("Player1AttackRoll: " + player1AttackStrength);
+            var player2AttackStrength = SumStrengthOfAPlayer(secondPlayerUnits, Moves.Attack);
+            Console.WriteLine("Player2AttackRoll: " + player2AttackStrength);
+
+            // Get DefenseStrength
+            var player1DefenseStrength = SumStrengthOfAPlayer(firstPlayerUnits, Moves.Defense);
+            Console.WriteLine("Player1DefenseRoll: " + player1DefenseStrength);
+
+            var player2DefenseStrength = SumStrengthOfAPlayer(firstPlayerUnits, Moves.Defense);
+            Console.WriteLine("Player2DefenseRoll: " + player2DefenseStrength);
+
+            // Calculate Attacks, Defense
+            var player1Attack = CalculateAttack(player1AttackStrength);
+            Console.WriteLine("Player1Attack: " + player1Attack);
+
+            var player2Attack = CalculateAttack(player2AttackStrength);
+            Console.WriteLine("Player2Attack: " + player2Attack);
+
+
+            var player1Defense = CalculateDefenseOrArmor(player1DefenseStrength);
+            Console.WriteLine("Player1Defense: " + player1Defense);
+
+            var player2Defense = CalculateDefenseOrArmor(player2DefenseStrength);
+            Console.WriteLine("Player2Defense: " + player2Defense);
+
+            // Calculate Hit
+            var player1Hit = player1Attack - player2Defense;
+
+            Console.WriteLine("Player1Hit: " + player1Hit);
+
+            var player2Hit = player2Attack - player1Defense;
+            Console.WriteLine("Player2Hit: " + player2Hit);
+
+
+            // Calculate Damage
+            CalculateDamageForAPLayer(firstPlayerUnits, player2Hit);
+            CalculateDamageForAPLayer(secondPlayerUnits, player1Hit);
+
+        }
+
+        private int SumStrengthOfAPlayer(List<Unit> units, Moves whatAreWeSuming)
+        {
+            int baseValue = 0;
+            int defendingUnitsNumber = 0;
+            foreach (var unit in units)
+            {
+                if (whatAreWeSuming == Moves.Attack)
+                {
+                    baseValue += unit.Attack;
+                }
+                if (whatAreWeSuming == Moves.Defense)
+                {
+                    baseValue += unit.Defense;
+                }
+                if (unit.CurrentMove == whatAreWeSuming)
+                {
+                    baseValue++;
+
+                    if (whatAreWeSuming == Moves.Attack)
+                    {
+                        baseValue++;
+                    }
+                }
+            }
+            if (whatAreWeSuming == Moves.Defense)
+            {
+                baseValue += defendingUnitsNumber;
+            }
+            return baseValue;
+        }
+
+        private int CalculateAttack(int attackStrength)
+        {
+            int attack = 0;
+            for (int i = 0; i < attackStrength; i++)
+            {
+                attack += random.Next(1, 6);
+            }
+            return attack;
+        }
+
+        private int CalculateDefenseOrArmor(int defenseStrength)
+        {
+            int defense = 0;
+            for (int i = 0; i < defenseStrength; i++)
+            {
+                defense += random.Next(1, 4);
+            }
+            return defense;
+        
+        }
+
+        private void CalculateDamageForAPLayer(List<Unit> units, int enemyHit)
+        {
+            foreach (var unit in units)
+            {
+                var resistance = unit.Armor;
+                if (unit.CurrentMove == Moves.Defense)
+                {
+                    resistance++;
+                }
+                resistance = CalculateDefenseOrArmor(resistance);
+                var damage = enemyHit - resistance;
+                Console.WriteLine("Player " + unit.UserId + "'s Damage: " + damage);
+                Console.WriteLine("Player " + unit.UserId + "'s Resistance: " + resistance);
+
+                // Damaging unit
+                unit.Hp -= damage;
+                Console.WriteLine("Player " + unit.UserId + "'s remaining HP is: " + unit.Hp);
+
+            }
+        }
+    }
+}
---
+++ BackendFiles/BattleCalculator/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BattleCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Console.WriteLine("Please enter the number of users: ");
+            var userNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter the number of unit for user 1#");
+            var user1NumberOFUnit = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < user1NumberOFUnit; i++)
+            {
+                var unit = new Unit(i);
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Attack value:");
+                unit.Attack = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Defense value:");
+                unit.Defense = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Special value:");
+                unit.Special = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Direction value:");
+                unit.Direction = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the attributes of the #" + i + "unit. Enter it's NewEntry true or false?:");
+                unit.NewEntry = bool.Parse(Console.ReadLine());
+
+                // mit csinálsz velük
+
+                Console.WriteLine("THANKS NEXT USER%%!!!!");
+            }
+
+            Console.WriteLine("Enter the number of unit for user 2#");
+            var user2NumberOFUnit = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < user2NumberOFUnit; i++)
+            {
+                var unit = new Unit(i);
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Attack value:");
+                unit.Attack = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Defense value:");
+                unit.Defense = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Special value:");
+                unit.Special = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the attributes of the #" + i + "unit, enter it's Direction value:");
+                unit.Direction = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the attributes of the #" + i + "unit. Enter it's NewEntry true or false?:");
+                unit.NewEntry = bool.Parse(Console.ReadLine());
+
+                // mit csinálsz velük
+                Console.WriteLine("THANKS NEXT USER%%!!!!");
+            }
+            */
+            var sampleUnit1 = new Unit(1);
+            var sampleUnit2 = new Unit(2);
+            var turn = 0;
+            var firstBattle = new Battle();
+            do
+            {
+                turn++;
+                firstBattle.SimulateBattle(new List<Unit>() { sampleUnit1 }, new List<Unit> { sampleUnit2 });
+                Console.WriteLine("Unit1's HP: " + sampleUnit1.Hp + " Unit2's HP: " + sampleUnit2.Hp);
+                Console.WriteLine("Turn #:  " + turn);
+                if (sampleUnit1.Hp <= 0)
+                {
+                    Console.WriteLine("Player 1 Died!");
+                }
+                if (sampleUnit2.Hp <= 0)
+                {
+                    Console.WriteLine("Player 2 Died!");
+                }
+            } while (sampleUnit1.Hp > 0 && sampleUnit2.Hp > 0);
+            Console.ReadLine();
+
+
+        }
+
+        public class Unit
+        {
+            public Unit(int userId)
+            {
+                UserId = userId;
+            }
+
+            public int Attack { get; set; } = 2;
+            public int Defense { get; set; } = 1;
+            public int Armor { get; set; } = 1;
+            public int Special { get; set; } = 1;
+            public int Hp { get; set; } = 10;
+            public int UserId { get; set; }
+            public string Egyeb { get; set; }
+            public bool NewEntry { get; set; } = false;
+            public int Direction { get; set; } = 1;
+            public Moves CurrentMove { get; set; } = Moves.Attack;
+
+        }
+
+        public enum Moves
+        {
+            Attack = 1,
+            Defense = 2,
+            Special = 3,
+            Retreat = 4
+        }
+        public enum Terrain
+        {
+            Plain = 1,
+            Mountain = 2,
+            Marsh = 3,
+            Camp = 4
+        }
+
+    }
+}
